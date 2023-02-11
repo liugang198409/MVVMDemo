507e7b1b1a28a08c99c70a02705be66c53b8e673
liugang <214818913@qq.com>
2023-02-11 21:44:32 +0800

    添加项目文件。

---
+++ CMDemo/Views/ShellView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CMDemo.Views
+{
+    /// <summary>
+    /// ShellView.xaml 的交互逻辑
+    /// </summary>
+    public partial class ShellView : Window
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
