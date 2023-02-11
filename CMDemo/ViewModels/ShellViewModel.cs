16c6857ad59ef16488b2343863d43a753009cf30
liugang <214818913@qq.com>
2023-02-11 21:44:32 +0800

    CMDemo添加项目文件。

---
+++ CMDemo/ViewModels/ShellViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace CMDemo.ViewModels
+{
+    public class ShellViewModel : Screen, INotifyPropertyChanged
+    {
+        private double _left;
+        private double _right;
+        private double _result;
+
+        public double Left
+        {
+            get { return _left; }
+            set
+            {
+                _left = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public double Right
+        {
+            get { return _right; }
+            set
+            {
+                _right = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public double Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                NotifyOfPropertyChange();
+            }
+        }
+        public bool CanDivide(double left, double right)
+        {
+            return right != 0;
+        }
+
+        public async void Divide(double left, double right)
+        {
+            Thread.Sleep(600);
+            if (CanDivide(left, right) == true)
+                Result = left / right;
+            else MessageBox.Show("Divider cannot be zero.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+
+        public async void Plus(double left, double right)
+        {
+            Result = left + right;
+        }
+
+        public async void Minus(double left, double right)
+        {
+            Result = left - right;
+        }
+
+        public async void Multipy(double left, double right)
+        {
+            Result = left * right;
+        }
+    }
+}
---
+++ PrismDemo/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrismDemo
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            //这里代表启动的view是MainWindow
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //注册类 这个Demo这里空着 不需要填写
+        }
+    }
+}
